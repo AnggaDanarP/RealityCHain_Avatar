91bba986249d3f62ae522ef0d8d9be70d9742644
AnggaDanarP <anggadanarp@gmail.com>
2022-04-27 13:54:21 +0700

    improve script

--- scripts/3_whitelist_close.ts
+++ scripts/3_whitelist_close.ts
@@ -1,8 +1,18 @@
+import { utils } from "ethers";
+import CollectionConfig from "../config/CollectionConfig";
 import NftContractProvider from "../lib/NftContractProvider";
 
 async function main() {
     // attach to deploy contract
     const contract = await NftContractProvider.getContract();
+
+    // get back to normal price (if needed)
+    const normalPrice = utils.parseEther(CollectionConfig.whitelistSale.price.toString());
+    if (!(await contract.cost()).eq(normalPrice)) {
+        console.log(`Updating the token price to ${CollectionConfig.whitelistSale.price} ${CollectionConfig.mainnet.symbol}...`);
+
+        await (await contract.setCost(normalPrice)).wait();
+    }
 
     // disable whitelist sale (if needed)
     if (await contract.whitelistMintEnable()) {
--- scripts/7_public_sale_close.ts
+++ scripts/7_public_sale_close.ts
@@ -1,14 +1,24 @@
+import { utils } from "ethers";
+import CollectionConfig from "../config/CollectionConfig";
 import NftContractProvider from '../lib/NftContractProvider';
 
 async function main() {
     // Attach to deployed contract
     const contract = await NftContractProvider.getContract();
 
+    // get back to normal price (if needed)
+    const normalPrice = utils.parseEther(CollectionConfig.whitelistSale.price.toString());
+    if (!(await contract.cost()).eq(normalPrice)) {
+        console.log(`Updating the token price to ${CollectionConfig.whitelistSale.price} ${CollectionConfig.mainnet.symbol}...`);
+
+        await (await contract.setCost(normalPrice)).wait();
+    }
+
     // Pause the contract (if needed)
     if (!await contract.paused()) {
-    console.log('Pausing the contract...');
+        console.log('Pausing the contract...');
 
-    await (await contract.setPaused(true)).wait();
+        await (await contract.setPaused(true)).wait();
     }
 
     console.log('Public sale is now closed!');
